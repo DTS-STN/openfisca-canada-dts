934706211aa9b9472fe49ed5c15071877651092d
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-08-10 13:38:06 -0400

    student_loan update

--- openfisca_canada/variables/benefits/student_loan/student_loan.py
+++ openfisca_canada/variables/benefits/student_loan/student_loan.py
@@ -6,5 +6,6 @@ class student_loan__is_paying_debt(Variable):
     value_type = bool
     entity = Person
     definition_period = MONTH
+    default_value = false
     label = "Students are given 6 months deferral"
     reference = "to be linked"
