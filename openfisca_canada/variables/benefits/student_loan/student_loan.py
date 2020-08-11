fd122ee81fca2109a3685921b149d8a38317f0b6
MarcoGoC <marco.flores@hrsdc-rhdcc.gc.ca>
2020-08-10 22:18:29 -0400

    Student_loan

--- openfisca_canada/variables/benefits/student_loan/student_loan.py
+++ openfisca_canada/variables/benefits/student_loan/student_loan.py
@@ -2,7 +2,7 @@ from openfisca_core.model_api import *
 from openfisca_canada.entities import Person
 
 
-class student_loan__is_paying_debt(Variable):
+class student_loan__has_student_debt(Variable):
     value_type = bool
     entity = Person
     definition_period = MONTH
