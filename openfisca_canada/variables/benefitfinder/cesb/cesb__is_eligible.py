c0951fd0dcc04c2cbd7a1931a951a9ff9f55363e
shawn320 <azuneko1110@gmail.com>
2020-08-12 11:34:39 -0400

    update cesb benefit

--- openfisca_canada/variables/benefitfinder/cesb/cesb__is_eligible.py
+++ openfisca_canada/variables/benefitfinder/cesb/cesb__is_eligible.py
@@ -3,12 +3,12 @@ from openfisca_core.model_api import *
 # Import the entities specifically defined for this tax and benefit system
 from openfisca_canada.entities import Person
 
-class high_school_grad(Variable):
+class cesb__is_eligible(Variable):
     value_type = bool
+    default_value = False
     entity = Person
     definition_period = MONTH
-    label = u"Is graduating from high school in the year of 2019-2020"
+    label = u"student is eligible for CESB benefit"
 
     def formula(persons, period, parameters):
-        return persons("no_income_before", period)
-
+        return persons("is_student_2019_2020", period) + persons("is_high_school_grad", period)
