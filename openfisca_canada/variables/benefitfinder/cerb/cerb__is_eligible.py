dac624259c654800a38c3262ea4fc2721345b8f7
MikeBungay <mikebungay76@gmail.com>
2020-08-13 12:20:38 -0400

    refactored income_status_reason to rename variable

--- openfisca_canada/variables/benefitfinder/cerb/cerb__is_eligible.py
+++ openfisca_canada/variables/benefitfinder/cerb/cerb__is_eligible.py
@@ -15,11 +15,11 @@ class cerb__is_eligible(Variable):
 
     def formula(persons, period, parameters):
         return (persons('income_status__has_lost_all_income', period) * (persons('income_status_reason__has_lost_job', period) + persons('income_status_reason__has_employer_closed', period) + \
-            persons('income_status_reason__has_self_employee_with_no_income', period) + persons('income_status_reason__has_unpaid_leave_to_care_for_child_or_sick', period) + \
+            persons('income_status_reason__is_self_employed', period) + persons('income_status_reason__has_unpaid_leave_to_care_for_child_or_sick', period) + \
             persons('income_status_reason__has_parental_recently_cant_return_to_work', period) + persons('income_status_reason__has_ei_recent_claim_ended', period))) + \
             (persons('income_status__has_lost_some_income', period) * ((persons("income_status_reason__has_hours_reduced",period) + persons("income_status_reason__employed_lost_a_job",period)) * persons("income_status_reason__has_1000_or_less",period))) + \
             (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_quarantined",period)) + \
-            (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_self_employed_some_income",period) * persons("income_status_reason__has_1000_or_less",period)) + \
+            (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_self_employed",period) * persons("income_status_reason__has_1000_or_less",period)) + \
             persons('income_status_reason__is_gross_income_over_5k', period)
 
 class cerb__eligible_scenario(Variable):
@@ -27,21 +27,21 @@ class cerb__eligible_scenario(Variable):
     entity = Person
     definition_period = MONTH
     default_value = False
-    label = u"is person eligible for cerb"
+    label = u"which scenario of cerb is person eligible?"
     reference = "tbd"
 
     def formula(persons, period, parameters):
         result = np.arange(1)
         if (persons('income_status__has_lost_all_income', period) * (persons('income_status_reason__has_lost_job', period) + persons('income_status_reason__has_employer_closed', period))):
             result[0]=1
-        elif (persons('income_status__has_lost_all_income', period) * persons('income_status_reason__has_self_employee_with_no_income', period) + persons('income_status_reason__has_unpaid_leave_to_care_for_child_or_sick', period) + \
+        elif (persons('income_status__has_lost_all_income', period) * persons('income_status_reason__is_self_employed', period) + persons('income_status_reason__has_unpaid_leave_to_care_for_child_or_sick', period) + \
             persons('income_status_reason__has_parental_recently_cant_return_to_work', period) + persons('income_status_reason__has_ei_recent_claim_ended', period)):
                 result[0]= 4
         elif (persons('income_status__has_lost_some_income', period) * ((persons("income_status_reason__has_hours_reduced",period) + persons("income_status_reason__employed_lost_a_job",period)) * persons("income_status_reason__has_1000_or_less",period))):
                 result[0]=  2
         elif (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_quarantined",period)):
                 result[0]=  3
-        elif  (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_self_employed_some_income",period) * persons("income_status_reason__has_1000_or_less",period)):
+        elif  (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_self_employed",period) * persons("income_status_reason__has_1000_or_less",period)):
             result[0]= 5
         elif persons('income_status_reason__is_gross_income_over_5k', period):
             result[0]= 6
