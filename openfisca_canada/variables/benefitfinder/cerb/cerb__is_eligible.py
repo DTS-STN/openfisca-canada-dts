f35fd49202b5f308c576d9cec33d9f420c99eccb
MikeBungay <mikebungay76@gmail.com>
2020-08-18 12:15:07 -0400

    Updated the CERB class based on comments from the Rocket Code Review

--- openfisca_canada/variables/benefitfinder/cerb/cerb__is_eligible.py
+++ openfisca_canada/variables/benefitfinder/cerb/cerb__is_eligible.py
@@ -2,8 +2,6 @@
 
 from openfisca_core.model_api import *
 from openfisca_canada.entities import Person, Family
-import numpy as np
-
 
 class cerb__is_eligible(Variable):
     value_type = bool
@@ -14,38 +12,83 @@ class cerb__is_eligible(Variable):
     reference = "tbd"
 
     def formula(persons, period, parameters):
-        return (persons('income_status__has_lost_all_income', period) * (persons('income_status_reason__has_lost_job', period) + persons('income_status_reason__has_employer_closed', period) + \
-            persons('income_status_reason__is_self_employed', period) + persons('income_status_reason__has_unpaid_leave_to_care_for_child_or_sick', period) + \
-            persons('income_status_reason__has_parental_recently_cant_return_to_work', period) + persons('income_status_reason__has_ei_recent_claim_ended', period))) + \
-            (persons('income_status__has_lost_some_income', period) * ((persons("income_status_reason__has_hours_reduced",period) + persons("income_status_reason__employed_lost_a_job",period)) * persons("income_status_reason__has_1000_or_less",period))) + \
-            (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_quarantined",period)) + \
-            (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_self_employed",period) * persons("income_status_reason__has_1000_or_less",period)) + \
-            persons('income_status_reason__is_gross_income_over_5k', period)
+        return persons('cerb__is_eligible_lost_all_income_lost_job_or_employer_closed', period) +\
+                persons('cerb__is_eligible_lost_some_income_reduced_hours_1000_or_less', period) +\
+                persons('cerb__is_eligible_lost_some_income_quarantine', period) +\
+                persons('cerb__is_eligible_lost_all_income_self_employed_closed_unpaid_leave_parental_leave_recent_ei_claim', period) +\
+                persons('cerb__is_eligible_lost_some_income_self_employed_1000_hours_or_less', period) +\
+                persons('cerb__is_eligible_gross_income_over_5k', period)
 
-class cerb__eligible_scenario(Variable):
-    value_type = float
+        
+class cerb__is_eligible_lost_all_income_lost_job_or_employer_closed(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"which scenario of cerb is person eligible?"
+    reference = "tbd"
+
+    def formula(persons, period, parameters):
+        return persons('income_status__has_lost_all_income', period) * (persons('income_status_reason__has_lost_job', period) +\
+            persons('income_status_reason__has_employer_closed', period))
+        
+class cerb__is_eligible_lost_some_income_reduced_hours_1000_or_less(Variable):
+    value_type = bool
     entity = Person
     definition_period = MONTH
     default_value = False
     label = u"which scenario of cerb is person eligible?"
     reference = "tbd"
-
+    
     def formula(persons, period, parameters):
-        result = np.arange(1)
-        if (persons('income_status__has_lost_all_income', period) * (persons('income_status_reason__has_lost_job', period) + persons('income_status_reason__has_employer_closed', period))):
-            result[0]=1
-        elif (persons('income_status__has_lost_all_income', period) * persons('income_status_reason__is_self_employed', period) + persons('income_status_reason__has_unpaid_leave_to_care_for_child_or_sick', period) + \
-            persons('income_status_reason__has_parental_recently_cant_return_to_work', period) + persons('income_status_reason__has_ei_recent_claim_ended', period)):
-                result[0]= 4
-        elif (persons('income_status__has_lost_some_income', period) * ((persons("income_status_reason__has_hours_reduced",period) + persons("income_status_reason__employed_lost_a_job",period)) * persons("income_status_reason__has_1000_or_less",period))):
-                result[0]=  2
-        elif (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_quarantined",period)):
-                result[0]=  3
-        elif  (persons('income_status__has_lost_some_income', period) * persons("income_status_reason__is_self_employed",period) * persons("income_status_reason__has_1000_or_less",period)):
-            result[0]= 5
-        elif persons('income_status_reason__is_gross_income_over_5k', period):
-            result[0]= 6
-        else:
-            result[0]=  0
-        return result
+        return persons('income_status__has_lost_some_income', period) * ((persons('income_status_reason__has_hours_reduced', period) + \
+            persons('income_status_reason__employed_lost_a_job', period)) * persons('income_status_reason__has_1000_or_less', period))
+
+class cerb__is_eligible_lost_some_income_quarantine(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    default_value = False
+    label = u"which scenario of cerb is person eligible?"
+    reference = "tbd"
+    
+    def formula(persons, period, parameters):
+        return persons('income_status__has_lost_some_income', period) * persons('income_status_reason__is_quarantined', period)
             
+
+
+class cerb__is_eligible_lost_all_income_self_employed_closed_unpaid_leave_parental_leave_recent_ei_claim(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    default_value = False
+    label = u"is person eligible for cerb__is_eligible_lost_all_income_self_employed_closed_unpaid_leave_parental_leave_recent_ei_claim?"
+    reference = "tbd"
+    
+    def formula(persons, period, parameters):
+        return persons('income_status__has_lost_all_income', period) * (persons('income_status_reason__is_self_employed', period) + \
+            persons('income_status_reason__has_unpaid_leave_to_care_for_child_or_sick', period) + \
+            persons('income_status_reason__has_parental_recently_cant_return_to_work', period) + \
+            persons('income_status_reason__has_ei_recent_claim_ended', period))
+
+class cerb__is_eligible_lost_some_income_self_employed_1000_hours_or_less(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    default_value = False
+    label = u"is person eligible for cerb__is_eligible_lost_some_income_self_employed_1000_hours_or_less?"
+    reference = "tbd"
+    
+    def formula(persons, period, parameters):
+        return persons('income_status__has_lost_some_income', period) * persons('income_status_reason__is_self_employed', period) * \
+            persons('income_status_reason__has_1000_or_less', period)
+
+class cerb__is_eligible_gross_income_over_5k(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    default_value = False
+    label = u"is person eligible fo cerb__is_eligible_gross_income_over_5k?"
+    reference = "tbd"
+    
+    def formula(persons, period, parameters):
+        return persons('income_status_reason__is_gross_income_over_5k', period)
