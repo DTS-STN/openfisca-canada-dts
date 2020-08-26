ff38b41eb4e4ce197b951c452cd4020849f71f62
shawn320 <azuneko1110@gmail.com>
2020-08-26 14:26:05 -0400

    rate of benefits

---
+++ openfisca_canada/variables/ei/rate_of_benefits/unemployment_benefits__rate_of_weekly_benefits.py
@@ -0,0 +1,24 @@
+# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the entities specifically defined for this tax and benefit system
+from openfisca_canada.entities import Person
+
+class unemployment_benefits__rate_of_weekly_benefits(Variable):
+    value_type = int
+    entity = Person
+    definition_period = Month
+    default_value = .55
+    label = 'rate of weekly ensurable earning'
+
+    def formula(persons, period, parameters):
+        if (parental_benefits__elected_for_extended_benefits):
+            return parameters(period).rate_of_benefits.unemployment_benefits__rate_of_weekly_benefits_parental_extended
+        else:
+            return parameters(period).rate_of_benefits.unemployment_benefits__rate_of_weekly_benefits
+
+class parental_benefits__elected_for_extended_benefits(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = Month
+    default_value = False
+    label: 'claimant elects to have parental benefits'
