84ba6343093e0b02e6c86a013f35b5f8df01b9c1
shawn320 <azuneko1110@gmail.com>
2020-08-28 09:31:59 -0400

    parental benefits

---
+++ openfisca_canada/variables/ei/parental_benefits/parental_benefits__is_eligible_for_parental_benefit.py
@@ -0,0 +1,37 @@
+# Import from openfisca-core the common python objects used to code the legislation in OpenFisca
+from openfisca_core.model_api import *
+# Import the entities specifically defined for this tax and benefit system
+from openfisca_canada.entities import Person
+
+class parental_benefits__is_caring_for_one_or_more_new_born_children(Variable):
+    value_type: bool
+    entity: Person
+    definition_period = MONTH
+    default_value = False
+    label = "claimant caring for one or more new born children"
+
+class parental_benefits__is_caring_for_one_or_more_adopted_children(Variable):
+    value_type: bool
+    entity: Person
+    definition_period = MONTH
+    default_value = False
+    label = "claimant caring for one or more adopted children"
+
+class unemployment_benefits__is_major_attached_claimant(Variable):
+    value_type: bool
+    entity: Person
+    definition_period = MONTH
+    default_value = False
+    label = "claimant is major attached claimant"
+
+class parental_benefits__is_eligible_for_parental_benefit(Variable):
+    value_type: bool
+    entity: Person
+    definition_period = MONTH
+    default_value = False
+    label = "claimant is eligible for parental benefit"
+
+    def formula(persons, period, parameters):
+        return persons('unemployment_benefits__is_major_attached_claimant', period) * /
+            (persons('parental_benefits__is_caring_for_one_or_more_new_born_children', period) + /
+            persons('parental_benefits__is_caring_for_one_or_more_adopted_children', period))
