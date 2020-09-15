501312fbf3783e51130f2b3284375dba96381e00
shewood <shewood01@gmail.com>
2020-09-15 13:07:53 -0400

    added scenario for dtc-apply

--- openfisca_canada/variables/benefitfinder/dtc/dtc__document.py
+++ openfisca_canada/variables/benefitfinder/dtc/dtc__document.py
@@ -3,9 +3,17 @@
 from openfisca_core.model_api import *
 from openfisca_canada.entities import Person, Family
 
-
+ 
 class dtc__has_documented_disability(Variable):
     value_type = bool
     entity = Person
     definition_period = MONTH
     label = u"has documented disability"
+    reference = u"TODO"
+
+class dtc__has_disability(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"has a disability but it is not documented"
+    reference = u"TODO"    
--- openfisca_canada/variables/benefitfinder/dtc/dtc__is_eligible.py
+++ openfisca_canada/variables/benefitfinder/dtc/dtc__is_eligible.py
@@ -2,7 +2,7 @@
 
 from openfisca_core.model_api import *
 from openfisca_canada.entities import Person, Family
-
+ 
 #Disability tax credit
 class dtc__is_eligible(Variable):
     value_type = bool
@@ -29,3 +29,21 @@ class dtc__is_eligible_for_dtc_and_oas(Variable):
     def formula(persons, period, parameters):
         return persons('dtc__has_documented_disability', period) *\
             persons('oas__is_eligible', period)
+
+class dtc_apply__is_eligible(Variable):
+    value_type = bool
+    entity = Person
+    definition_period = MONTH
+    label = u"Is person eligible for a disibility tax credit"
+    end = "2020-09-30"
+
+    def formula(persons, period, parameters):
+        #if persons.family.nb_persons(role=Family.CHILD).any():
+        if persons.has_role(Family.PARENT).any():
+            return persons('dtc__has_disability', period) *\
+                not_(persons('dtc__has_documented_disability', period)) +\
+                persons.family.any(persons.family.members('dtc__has_disability', period), role=Family.CHILD) *\
+                not_(persons.family.any(persons.family.members('dtc__has_documented_disability', period), role=Family.CHILD))
+        else:
+            return persons('dtc__has_disability', period) *\
+                not_(persons('dtc__has_documented_disability', period))          
