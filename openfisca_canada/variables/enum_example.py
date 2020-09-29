7e2ffc3e6fbf7f829df9ff2e317bef297b237413
shawn320 <azuneko1110@gmail.com>
2020-09-29 12:23:04 -0400

    enum

--- openfisca_canada/variables/enum_example.py
+++ openfisca_canada/variables/enum_example.py
@@ -2,24 +2,52 @@ from openfisca_core.model_api import *
 from openfisca_canada.entities import *
 
 # Enumerations are variables that holding a limited set of possible values.
-# Openfisca enums are based on Python 3 native enums. Each enum item (for instance: LostAllIncom.lost_job) has:
-# - a name attribute that contains its key (e.g lost_job)
-# - a value attribue that contains its description (e.g "has lost job")
-class LostAllIncome(Enum):
-    lost_job = u"has lost job"
-    employer_closed = u"has employer closed"
-    self_employed = u"is self employed and have no income"
-    school_closed = u"has child with closed school"
-    unpaid_leave = u"has unpaid leave to care for child or sick"
-    quarantined = u"is sick and quarantined"
-    parental_leave = u"on parental leave recently, but cannot return to work"
-    student = u"is student during 2019-2020 and cannot find work"
+# Openfisca enums are based on Python 3 native enums. Each enum item (for instance: IncomeStatus.all_income) has:
+# - a name attribute that contains its key (e.g all_income)
+# - a value attribue that contains its description (e.g "lost all income")
+class IncomeStatus(Enum):
+    ALL_INCOME = "lost all income"
+    SOME_INCOME = "lost some income"
+    UNCHANGED_INCOME = "lost no income "
+    UNKNOWN = "unknown"
 
-class lost_all_income(Variable):
+class BooleanEnum(Enum):
+    TRUE = "true"
+    FALSE = "false"
+    UNKNOWN = "unknown"
+    
+class income_status(Variable):
     value_type = Enum
-    possible_values = LostAllIncome
-    default_value = LostAllIncome.lost_job # The default is mandatory
+    possible_values = IncomeStatus
+    default_value = IncomeStatus.UNKNOWN # The default is mandatory
     entity = Person
     definition_period = MONTH
+    label = "the input variable that to be compared with the enum value"
+
+
+class cerb_is_eligible(Variable):
+    value_type = Enum
+    possible_values = BooleanEnum
+    default_value = BooleanEnum.UNKNOWN
+    entity = Person
+    definition_period = MONTH
+    label = "testing enum scenerio"
+
+    def formula(persons, period, parameters):
+        income_status = persons('income_status', period)
+        lost_all_income = (income_status == IncomeStatus.ALL_INCOME)
+        lost_some_income = (income_status == IncomeStatus.SOME_INCOME)
+        lost_no_income = (income_status == IncomeStatus.UNCHANGED_INCOME)
+        unknown = (income_status == IncomeStatus.UNKNOWN)
+
+        return (lost_all_income + lost_some_income + lost_no_income + unknown)
+
+        '''if ((lost_all_income + lost_some_income).any()):
+            return BooleanEnum.TRUE
+        elif (lost_no_income):
+            return BooleanEnum.FALSE
+        else:
+            return BooleanEnum.UNKNOWN'''
+
 
 
