d51cac8a7863ec1c3b9f0d83214ccd4d29292806
MikeBungay <mikebungay76@gmail.com>
2020-11-03 14:11:30 -0500

    upgradesd libraries and add DOB calculation (Still not working properly)

--- openfisca_canada/variables/EBF/benefit_eligibility/drivers_license.py
+++ openfisca_canada/variables/EBF/benefit_eligibility/drivers_license.py
@@ -1,5 +1,5 @@
 
-from openfisca_core.model_api import *
+from openfisca_core import *
 from openfisca_canada.entities import Person
 from openfisca_canada.variables.common.provinces import *
 
@@ -8,16 +8,17 @@ class drivers_license__is_eligible(Variable):
     value_type = bool
     default_value = False
     entity = Person
-    definition_period = MONTH
+    definition_period = DAY
     label = u"Person is eligible for a drivers license"
     
     def formula(persons, period, parameters):
-        person_province = persons("province_of_residence", period)
+        month = period.this_month
+        person_province = persons("province_of_residence", month)
         person_16_or_older = persons("is_16_or_older", period)
-        person_is_canadian_resident = persons("is_canadian_resident",period)
-        person_has_completed_driver_course = persons("has_completed_driver_training", period)
-        person_has_parental_consent = persons("has_parental_consent",period)
-        person_is_expiring_2021 = persons("is_expiring_2021",period)
+        person_is_canadian_resident = persons("is_canadian_resident",month)
+        person_has_completed_driver_course = persons("has_completed_driver_training", month)
+        person_has_parental_consent = persons("has_parental_consent",month)
+        person_is_expiring_2021 = persons("is_expiring_2021",month)
         
         return ( person_province != CanadianProvinceOrTerritory.UNKNOWN) *\
             person_16_or_older * person_is_canadian_resident * person_has_completed_driver_course *\
--- openfisca_canada/variables/EBF/eligibility_criteria/person_age.py
+++ openfisca_canada/variables/EBF/eligibility_criteria/person_age.py
@@ -8,7 +8,7 @@ class is_16_or_older(Variable):
     value_type = bool
     default_value = False
     entity = Person
-    definition_period = MONTH
+    definition_period = DAY
     label = u"Person is 16 years of age and older"
 
     def formula(persons, period, parameters):
@@ -17,19 +17,22 @@ class is_16_or_older(Variable):
 
 class age(Variable):
     value_type = int
-    default_value = date.today()
     entity = Person
-    definition_period = MONTH
+    definition_period = DAY
     label = u"Person's date of birth"
     def formula(persons, period, parametes):
-        today = date.today()
-        birthdate = persons("date_of_birth", period)[0].astype(datetime)
-        return birthdate < today * (today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day)))
+        birthdate = persons("date_of_birth", period)
+        birth_year = birthdate.astype('datetime64[Y]').astype(int) + 1970
+        birth_month = birthdate.astype('datetime64[M]').astype(int) % 12 + 1
+        birth_day = (birthdate - birthdate.astype('datetime64[M]') + 1).astype(int)
+        is_birthday_past = (birth_month < period.start.month) + (birth_month == period.start.month) * (birth_day <= period.start.day)
+        return (period.start.year - birth_year) - where(is_birthday_past, 0, 1)  # If the birthday is not passed this year, substract one year
 
 class date_of_birth(Variable):
     value_type = date
+    default_value = date.today()
     entity = Person
-    definition_period = ETERNITY
+    definition_period = ETERNITY # This variable cannot change over time.
     label = u"Person's date of birth"
 
   
