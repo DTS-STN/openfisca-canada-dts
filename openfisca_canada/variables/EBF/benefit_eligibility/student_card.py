bd5712f566b9c3fe93237d20f94e4ebe08d5402e
shewood <shewood01@gmail.com>
2020-11-09 12:07:16 -0500

    added student card tests

--- openfisca_canada/variables/EBF/benefit_eligibility/student_card.py
+++ openfisca_canada/variables/EBF/benefit_eligibility/student_card.py
@@ -12,11 +12,11 @@ class student_card__is_eligible(Variable):
     
     def formula(persons, period, parameters):
         person_province = persons("province_of_residence", period)
-        person_16_or_older = persons("is_18_or_younger", period)
+        person_is_18_or_younger = persons("is_18_or_younger", period)
         person_is_canadian_resident = persons("is_canadian_resident",period)
         person_has_parental_consent = persons("has_parental_consent",period)
         person_is_expiring_2021 = persons("is_expiring_2021",period)
         
         return ( person_province != CanadianProvinceOrTerritory.UNKNOWN) *\
-            person_18_or_younger * person_is_canadian_resident *\
+            person_is_18_or_younger * person_is_canadian_resident *\
             person_has_parental_consent * person_is_expiring_2021
